d3d261a198bdbd9529337ae8de450fbe93706a6b
Aldo Adirajasa Fathoni <aldo.alfathoni@gmail.com>
2024-03-25 14:37:04 +0700

    Add generator script

---
+++ generate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+dir=$1
+url=$2
+
+mkdir $dir
+cat > $dir/index.html <<EOF
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to $url</title>
+<meta http-equiv="refresh" content="0; URL=$url">
+<link rel="canonical" href="$url">
+EOF
